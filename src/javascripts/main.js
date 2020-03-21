4546a6d5995ebfbb7a09680bba7d1fe9d63ca68f
David Everett <davideverett1989@gmail.com>
2020-03-21 13:48:44 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,17 @@
-import '../styles/main.scss';
-import 'bootstrap';
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 
-console.error('hi');
+import authData from './helpers/data/authData';
+import auth from './components/auth/auth';
+import myNavbar from './components/myNavBar/myNavBar';
+
+import '../styles/main.scss';
+
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  myNavbar.logoutEvent();
+};
+
+init();
