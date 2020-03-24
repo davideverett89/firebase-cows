0bbb744048893202fd656e880ac562fe9db920d5
David Everett <davideverett1989@gmail.com>
2020-03-23 19:22:04 -0500

    I see cows.

---
+++ src/javascripts/components/cow/cow.js
@@ -0,0 +1,15 @@
+const cowMaker = (cow) => {
+  let domString = '';
+  domString += '<div class="card">';
+  domString += `<div class="card-header">${cow.name}</div>`;
+  domString += '<div class="card-body">';
+  domString += `<h5 class="card-title">${cow.breed}</h5>`;
+  domString += `<p>${cow.weight}</p>`;
+  domString += `<p>${cow.location}</p>`;
+  domString += '</div>';
+  domString += '</div>';
+  domString += '</div>';
+  return domString;
+};
+
+export default { cowMaker };
